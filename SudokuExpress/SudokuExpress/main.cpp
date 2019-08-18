27fbdb4050e4f3fe8b0b8b49d7a5abfc33df2073
Julián <julianmangut@gmail.com>
2019-08-18 18:17:17 +0200

    FIrst change

---
+++ SudokuExpress/SudokuExpress/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+
+	int suma = 0;
+
+	for (int i = 0; i < 5; i++) {
+		suma++;
+	}
+
+	cout << "Hola mundo" << endl;
+	cout << suma << endl;
+}
